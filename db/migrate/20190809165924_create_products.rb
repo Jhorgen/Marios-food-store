0c059961e37d2a4f7e2eaeca2819813dd47246cf
Josh <Joshua.horgen@gmail.com>
2019-08-09 12:06:33 -0700

    Delete old migrations

--- db/migrate/20190809165924_create_products.rb
+++
@@ -1,9 +0,0 @@
-class CreateProducts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :products do |t|
-      t.string :name
-      t.integer :cost
-      t.string :country_of_origin
-    end
-  end
-end
