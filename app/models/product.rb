d7be37600b0e9bff18ce817b7bbf174938677a2c
Josh <Joshua.horgen@gmail.com>
2019-08-16 10:46:14 -0700

    Create specs, add landing page.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ApplicationRecord
-  has_many :reviews, dependent: :destroy
+  has_many :reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'products#index'
+  root to: 'landing#index'
 resources :products do
   resources :reviews
 end
