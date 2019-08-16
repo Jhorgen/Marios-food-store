a53b23bb3cc11781bd0bf30f58995c30ebb5bd02
Josh <Joshua.horgen@gmail.com>
2019-08-16 16:29:17 -0700

    Working on routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
-  has_many :reviews
-
+   has_many :reviews, dependent: :destroy
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'landing#index'
 resources :products do
   resources :reviews
 end
+root to: 'landing#index'
 
 end
