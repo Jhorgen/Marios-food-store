a62136f931aee091d3c1ef3c87c5003ef589f395
Josh <Joshua.horgen@gmail.com>
2019-08-24 21:31:39 -0700

    Fix specs and add more CRUD testing.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -12,7 +12,7 @@ it("should create a review for a product") do
   product.save!
   review = product.reviews.create({author: "hunter s. thompson", content_body: "Lorem ipsum dolar", rating: 3})
   review.save!
-  expect(review).to(eq(review))
+  expect(Review.all).to(eq([review]))
   end
 
   it("should edit a review") do
@@ -22,5 +22,16 @@ it("should create a review for a product") do
     review.save!
     review.update!({author: "Josh"})
     expect(review.author).to(eq("Josh"))
+  end
+
+  it("should delete a review") do
+    product = Product.create({name: "green beans", cost: 5.00, country_of_origin: "USA"})
+    product.save!
+    review1 = product.reviews.create({author: "hunter s. thompson", content_body: "Lorem ipsum dolar", rating: 3})
+    review1.save!
+    review2 = product.reviews.create({author: "Josh", content_body: "Lorem ipsum dolar", rating: 5})
+    review2.save!
+    review1.destroy
+    expect(Review.all).to(eq([review2]))
   end
 end
