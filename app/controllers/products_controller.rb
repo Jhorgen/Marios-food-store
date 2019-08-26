56017740cdc5176b7436dd5612d49e9d1a58238f
Josh <Joshua.horgen@gmail.com>
2019-08-25 23:02:30 -0700

    Add integration testing for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     if @product.save
       flash[:notice] = "Product successfully added!"
-      redirect_to products_path
+      redirect_to product_path(@product)
     else
       render :new
     end
--- spec/integration/product_spec.rb
+++ spec/integration/product_spec.rb
@@ -7,6 +7,48 @@ RSpec.describe "Products", type: :feature do
   expect(page).to have_content 'Most Reviewed Products'
   expect(page).to have_content 'Recently Reviewed'
   expect(page).to have_content 'American made products'
+  end
 
+  it "adds a new product" do
+  visit '/'
+  click_link 'View all Products'
+  click_link 'Add a product'
+  fill_in 'Name', :with => 'tester'
+  fill_in 'Cost', :with => '16'
+  fill_in 'Country of origin', :with => 'Guam'
+  click_on 'Create Product'
+  expect(page).to have_content 'Product successfully added!'
+  expect(page).to have_content 'Tester'
+  expect(page).to have_content '$16'
+  expect(page).to have_content 'Guam'
+end
+
+  it "edits a products' details" do
+    visit '/'
+    click_link 'View all Products'
+    click_link 'Add a product'
+    fill_in 'Name', :with => 'tester'
+    fill_in 'Cost', :with => '16'
+    fill_in 'Country of origin', :with => 'Guam'
+    click_on 'Create Product'
+    click_on 'Edit'
+    fill_in 'Name', :with => 'newtest'
+    fill_in 'Cost', :with => '5'
+    click_on 'Update Product'
+    expect(page).to have_content 'Newtest'
+    expect(page).to have_content '$5'
+    expect(page).to have_content 'Guam'
+  end
+
+    it "deletes a product" do
+  visit '/'
+  click_link 'View all Products'
+  click_link 'Add a product'
+  fill_in 'Name', :with => 'tester'
+  fill_in 'Cost', :with => '16'
+  fill_in 'Country of origin', :with => 'Guam'
+  click_on 'Create Product'
+  click_link 'Delete product'
+  expect(page).to_not have_content 'Tester'
   end
 end
